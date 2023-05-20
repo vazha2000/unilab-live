7ece3172f2f524ea2402a5a91dee2989b729e885
vazha2000 <vaja.siqtura2000@gmail.com>
2023-05-20 23:00:33 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [circle, setCircle] = useState({
+    xPosition: null,
+    yPosition: null,
+    // colors: ["red", "green", "blue", "yellow", "white", "purple", "black", "orange", "slate"],
+    color: "yellow"
+  });
+  const [circleHistory, setCircleHistory] = useState([]);
+  // const [circleIndex, setCircleIndex] = useState(0);
+
+  const handleAddCircle = (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+    console.log(x);
+    console.log(y);
+    setCircle({ xPosition: x, yPosition: y, colors: "yellow"  });
+    setCircleHistory([...circleHistory, { xPosition: x, yPosition: y, }]);
+  }
+  const undo = () => {
+    setCircleHistory(circleHistory.pop());
+    console.log(circleHistory)
+  };
+
+  const redo = () => {
+    setCircleHistory(circleHistory.push(circle))
+    console.log(circleHistory)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-container" onClick={handleAddCircle}>
+        <button onClick={undo}>undo</button>
+        <button onClick={redo}>redo</button>
+      </div>
+      {circle && (
+        <div
+          style={{
+            position: "absolute",
+            top: circle.yPosition,
+            left: circle.xPosition,
+            backgroundColor: 'yellow'
+          }}
+          className="circle"
+        ></div>
+      )}
     </div>
   );
 }
